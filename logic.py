7ed08c6b8d8fe463eb7435ed536c856bd73f001a
ErrorinLine30 <Linus.gerhard@web.de>
2025-11-24 10:38:07 +0100

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,47 @@
+# Nicht alle Imports werden aktuell verwendet.
+# Diese können in Zukunft aber sehr nützlich sein
+# und zeigen außerdem alle Funktionalitäten der Software-Challenge Python API.
+import math
+import random
+import time
+from typing import Optional, Tuple
+import pyautogui
+from socha import (
+    Coordinate,
+    Vector,
+    Direction,
+    FieldType,
+    TeamEnum,
+    Board,
+    Move,
+    GameState,
+    RulesEngine,
+)
+from socha.api.networking.game_client import IClientHandler
+from socha.starter import Starter
+
+
+class Logic(IClientHandler):
+    game_state: GameState
+
+    # Diese Methode wird immer aufgerufen, wenn der Spielserver einen Zug vom Client anfordert.
+    # Sie muss implementiert sein, weil der Computerspieler sonst disqualifiziert wird.
+    # Damit ein Zug an den Spielserver übermittelt wird, muss dieser mit *return* von der Methode zurückgegeben werden.
+    def calculate_move(self) -> Move:
+        print(self.game_state.possible_moves())
+        pyautogui.moveTo(1500,110)
+        pyautogui.click()
+        return random.choice(self.game_state.possible_moves())
+
+    # Diese Methode wird jedes Mal aufgerufen, wenn der Server einen neunen Spielstand bereitstellt.
+    # Sie muss implentiert sein, damit die Spielstand Instanz auf dem neusten Stand bleibt.
+    def on_update(self, state: GameState) -> None:
+        self.game_state = state
+
+    # Die Klasse IClientHandler hält noch weitere Methoden, die durch bestimmte Aktionen des Servers ausgeführt werden.
+    # Weitere Informationen dazu gibt es in der verlinkten Dokumentation unter dem Submodul socha.api.networking.game_client.
+
+if __name__ == "__main__":
+    Starter(logic=Logic())
+    # Wenn man mehr Debug Informationen aus dem Hintergrund der API haben möchte, kann das Log-Level auf debug gesetzt werden:
+    # Starter(logic=Logic(), log_level=logging.DEBUG)
